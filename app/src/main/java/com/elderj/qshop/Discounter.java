5e0a237363fe5a0b4d5361dfff0efb3d90908311
Jo <parusmontanus@gmail.com>
2017-09-20 18:23:07 +0100

    Discounter passing test - applies bogof when quantity odd

--- app/src/main/java/com/elderj/qshop/Discounter.java
+++ app/src/main/java/com/elderj/qshop/Discounter.java
@@ -12,13 +12,15 @@ public class Discounter implements Discounting {
 
                 break;
             case BOGOF:
+                double priceBeforeDiscount = product.buyPrice * quantity;
                 if (quantity % 2 == 0) {
                     return ((product.buyPrice * quantity) / 2);
+                } else {
+                    int discountQuantity = (quantity - 1) / 2;
+                    double discountAmount = discountQuantity * product.buyPrice;
+
+                    return priceBeforeDiscount - discountAmount;
                 }
-
-
-                break;
-
 
 
         }
