b651f1cfdf275a0389c9cc1559532fa8c3758ba8
Jo <parusmontanus@gmail.com>
2017-09-20 17:46:58 +0100

    Testing discounter applies bogof discount

---
+++ app/src/test/java/com/elderj/qshop/DiscounterTest.java
@@ -0,0 +1,23 @@
+package com.elderj.qshop;
+
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.hamcrest.core.IsEqual.equalTo;
+import static org.junit.Assert.*;
+
+public class DiscounterTest {
+
+    private Discounting discounter;
+
+    @Test
+    public void discounter_applies_bogof_discount_when_quantity_is_even() {
+        discounter = new Discounter();
+        Product product = new Product("", 1.0, 2.0, Discount.BOGOF);
+
+        double actualBuyCost = discounter.getBuyCost(product, 2);
+
+        assertThat(1.0, is(equalTo(actualBuyCost)));
+    }
+
+}
