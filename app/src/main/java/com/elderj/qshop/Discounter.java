3d4e11e848b7818215b9679ca71e9644775c48df
Jo <parusmontanus@gmail.com>
2017-09-20 17:53:31 +0100

    Discounter passing test - applies bogof when quantity even

--- app/src/main/java/com/elderj/qshop/Discounter.java
+++ app/src/main/java/com/elderj/qshop/Discounter.java
@@ -4,7 +4,25 @@ public class Discounter implements Discounting {
 
     public Discounter() {}
 
+
+
     public double getBuyCost(Product product, int quantity) {
+        switch (product.discount) {
+            case NONE:
+
+                break;
+            case BOGOF:
+                if (quantity % 2 == 0) {
+                    return ((product.buyPrice * quantity) / 2);
+                }
+
+
+                break;
+
+
+
+        }
+
 
         return 0.0;
     }
