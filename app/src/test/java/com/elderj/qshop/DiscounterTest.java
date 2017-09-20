34fec4250ae0975d900404e99df1840185aab861
Jo <parusmontanus@gmail.com>
2017-09-20 18:18:40 +0100

    Testing discounter applies bogof when quantity odd

--- app/src/test/java/com/elderj/qshop/DiscounterTest.java
+++ app/src/test/java/com/elderj/qshop/DiscounterTest.java
@@ -20,4 +20,15 @@ public class DiscounterTest {
         assertThat(1.0, is(equalTo(actualBuyCost)));
     }
 
+    @Test
+    public void discounter_applies_bogof_when_quantity_is_odd() {
+        discounter = new Discounter();
+        Product product = new Product("", 1.0, 2.0, Discount.BOGOF);
+
+        double actualBuyCost = discounter.getBuyCost(product, 7);
+
+        assertThat(4.0, is(equalTo(actualBuyCost)));
+    }
+
+
 }
